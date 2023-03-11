d8b92b633433d735924d9d09f2a64b56e70b1787
babi <babi@babis-MacBook.local>
2023-03-11 15:35:03 +0100

    faire sauter strapi check

--- src/lib/strapi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const API_URL = "http://127.0.0.1:1337/api";
-
-export const fetchAPI = async (path) => {
-	const url = `${API_URL}${path}`;
-	const response = await axios.get(url);
-	return response.data;
-};
